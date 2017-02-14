b42d5389e884b4927e0c2980241eb892fcce1cac
Nathanael Araujo <Nathanael.Araujo@CSSP-571.local>
2017-02-14 15:36:25 -0200

    Uploading api testing project
    Signed-off-by: Nathanael Araujo <Nathanael.Araujo@CSSP-571.local>

---
+++ README.md
@@ -0,0 +1,44 @@
+# api-testing-ruby-httparty
+
+Hey,
+
+Let's go to create a simple project for API Testing.
+First of all, you have downloading/clone this project for your PC and to install all dependencies. Okay?
+
+Do you have bundler installed? If not, you can know how to install in this link: http://bundler.io/
+It simplify your life for installing all dependencies.
+
+# Dependencies
+
+gem "httparty"
+gem "rspec"
+
+# How to install
+
+git clone https://github.com/nathsilv/api-testing-ruby-httparty.git
+bundle install
+
+Now you have this project on your PC and all dependencies are installed.
+Let's understand  each file!
+
+# FILE: GemFile
+
+In this file we have all dependencies that we need for testing our api!
+Any dependency that you need you can put in this file, It will help when exporting your project!
+
+# FILE: api-testing.rb
+
+Here, we have all tests that we need for testing our api(POST, GET, PUT and DELETE).
+In this project i am testing a API that i have created and you can see here: https://github.com/nathsilv/api-node
+This api is about series and it is simple!
+For you testing this project, you will need the API, so downloading that api-node project too! and Enjoy!
+
+# How to run my tests?
+
+First, up your API server(In my api-node project in my github i show how to up the server if you don't know it)
+Then, in the project directory execute: rspec api-testing.rb
+Simple, Isn't It?
+
+Any doubt you have, you can contact me and let's go to talk about it!
+
+Don't stay stopped here, go to learn more and more!
---
+++ api-testing.rb
@@ -0,0 +1,155 @@
+require "httparty"
+
+class TestAPI
+  include HTTParty
+  base_uri 'http://localhost:3000/api'
+end
+
+initial_serie = {
+  "genre" => "Acao",
+  "season" => "5",
+  "year" => "2012",
+  "name" => "Arrow"
+}
+response_global = TestAPI.post('/series', body: initial_serie)
+
+
+
+RSpec.describe 'API Testing  - POST' do
+  it 'It is not possible create serie without name' do
+    new_serie = {
+      'year' => '2011',
+      'season' => '7',
+      'genre' => 'Drama'
+    }
+    begin
+      response = TestAPI.post('/series', body: new_serie);
+      expect(response.code).to eql(400)
+      expect(response['message']).to eql('Missing required property: name/year/season or genre')
+    end
+  end
+
+  it 'It is not possible create serie without year' do
+    new_serie = {
+      'name' => 'Game of Thrones',
+      'season' => '7',
+      'genre' => 'Drama'
+    }
+    begin
+      response = TestAPI.post('/series', body: new_serie);
+      expect(response.code).to eql(400)
+      expect(response['message']).to eql('Missing required property: name/year/season or genre')
+    end
+  end
+
+  it 'It is not possible create serie without season' do
+    new_serie = {
+      'name' => 'Game of Thrones',
+      'year' => '2011',
+      'genre' => 'Drama'
+    }
+    begin
+      response = TestAPI.post('/series', body: new_serie);
+      expect(response.code).to eql(400)
+      expect(response['message']).to eql('Missing required property: name/year/season or genre')
+    end
+  end
+
+  it 'It is not possible create serie without genre' do
+    new_serie = {
+      'name' => 'Game of Thrones',
+      'year' => '2011',
+      'season' => '7',
+    }
+    begin
+      response = TestAPI.post('/series', body: new_serie);
+      expect(response.code).to eql(400)
+      expect(response['message']).to eql('Missing required property: name/year/season or genre')
+    end
+  end
+
+  it 'It is possible create serie' do
+    new_serie = {
+      'name' => 'New Serie',
+      'year' => '2011',
+      'season' => '7',
+      'genre' => 'New genre'
+    }
+    begin
+      response = TestAPI.post('/series', body: new_serie);
+      expect(response.code).to eql(201)
+      expect(response['name']).to eql(new_serie['name'])
+      expect(response['year']).to eql(new_serie['year'])
+      expect(response['season']).to eql(new_serie['season'])
+      expect(response['genre']).to eql(new_serie['genre'])
+    end
+  end
+end
+
+RSpec.describe 'API Testing  - GET' do
+  it 'Should to return a serie' do
+
+    begin
+      response = TestAPI.get('/series/' + response_global["_id"])
+      expect(response.code).to eql(200)
+      expect(response['name']).to eql(initial_serie['name'])
+      expect(response['year']).to eql(initial_serie['year'])
+      expect(response['season']).to eql(initial_serie['season'])
+      expect(response['genre']).to eql(initial_serie['genre'])
+    end
+  end
+
+  it 'Should to return HttpStatus 404 - NOT FOUND' do
+    begin
+      response = TestAPI.get('/series/notExist')
+      expect(response.code).to eql(404)
+      expect(response['message']).to eql('Not Found')
+    end
+  end
+end
+
+RSpec.describe 'API Testing - PUT' do
+  it 'Should update a serie' do
+    new_serie = {
+      'name' => 'Game of Thrones',
+      'year' => '2011',
+      'season' => '7',
+      'genre' => 'Drama'
+    }
+    begin
+      response = TestAPI.put('/series/' + response_global["_id"])
+      expect(response.code).to eql(204)
+    end
+  end
+
+  it 'Should to return HttpStatus 404 - NOT FOUND' do
+    new_serie = {
+      'name' => 'Game of Thrones',
+      'year' => '2011',
+      'season' => '7',
+      'genre' => 'Drama'
+    }
+    begin
+      response = TestAPI.put('/series/notExist',  body: new_serie)
+      expect(response.code).to eql(404)
+      expect(response['message']).to eql('Not Found')
+    end
+  end
+end
+
+RSpec.describe 'API Testing - DELETE' do
+  it 'Should delete a serie' do
+    begin
+      response = TestAPI.delete('/series/' + response_global["_id"])
+      expect(response.code).to eql(204)
+    end
+  end
+
+  it 'Should to return HttpStatus 404 - NOT FOUND' do
+    begin
+      response = TestAPI.get('/series/notExist')
+      expect(response.code).to eql(404)
+      expect(response['message']).to eql('Not Found')
+    end
+  end
+end
